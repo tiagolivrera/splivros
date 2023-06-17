5c97ba0be0e36839d2fa735e0fde1583637acb0c
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-06-17 17:52:25 -0300

    properties, livros, ORM

---
+++ src/main/java/br/com/tiagolivrera/splivros/entities/Livro.java
@@ -0,0 +1,133 @@
+package br.com.tiagolivrera.splivros.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_LIVRO")
+public class Livro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String genero;
+	private String autor;
+	private String idioma;
+	private String editora;
+	private Integer paginas;
+	private String sinopse;
+	private String imgUrl;
+
+	public Livro() {
+	}
+
+	public Livro(Long id, String titulo, String genero, String autor, String idioma, String editora, Integer paginas,
+			String sinopse, String imgUrl) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.genero = genero;
+		this.autor = autor;
+		this.idioma = idioma;
+		this.editora = editora;
+		this.paginas = paginas;
+		this.sinopse = sinopse;
+		this.imgUrl = imgUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public Integer getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(Integer paginas) {
+		this.paginas = paginas;
+	}
+
+	public String getSinopse() {
+		return sinopse;
+	}
+
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
