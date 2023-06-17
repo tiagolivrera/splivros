fb9ff85cf8641c6b18b5a5171d1eb58db7033ce2
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-06-17 17:21:27 -0300

    First commit

---
+++ src/main/java/br/com/tiagolivrera/splivros/SplivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagolivrera.splivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplivrosApplication.class, args);
+	}
+
+}
