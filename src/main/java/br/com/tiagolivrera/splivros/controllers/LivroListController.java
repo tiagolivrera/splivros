4f70d32274605218b5aa7d6f437cb566b5435c3b
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-06-18 15:09:53 -0300

    get livros by id

--- src/main/java/br/com/tiagolivrera/splivros/controllers/LivroListController.java
+++ src/main/java/br/com/tiagolivrera/splivros/controllers/LivroListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tiagolivrera.splivros.dto.LivroListDTO;
+import br.com.tiagolivrera.splivros.dto.LivroMinDTO;
 import br.com.tiagolivrera.splivros.services.LivroListService;
+import br.com.tiagolivrera.splivros.services.LivroService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -17,9 +20,18 @@ public class LivroListController {
 	@Autowired
 	private LivroListService livroListService;
 
+	@Autowired
+	private LivroService livroService;
+
 	@GetMapping
 	public List<LivroListDTO> findAll() {
 		List<LivroListDTO> result = livroListService.findAll();
+		return result;
+	}
+
+	@GetMapping(value = "/{listId}/livros")
+	public List<LivroMinDTO> findByList(@PathVariable Long listId) {
+		List<LivroMinDTO> result = livroService.findByList(listId);
 		return result;
 	}
 }
